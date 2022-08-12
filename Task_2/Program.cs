d24592c022920370dec9f106adf83641b1da25f7
Ichtyander <di_ignatov@mail.ru>
2022-08-12 14:33:29 +0300

    Добавьте файлы проекта.

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*2.    Необходимо разработать программу для получения информации о товаре из json-файла.
+Десериализовать файл «Products.json» из задачи 1. Определить название самого дорогого товара.*/
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
